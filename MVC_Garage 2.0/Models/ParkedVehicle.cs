13d329352d3932e90e40137868c4ca920beb5286
Lars Börlin <lars.borlin@gmail.com>
2017-10-23 11:16:43 +0200

    Add ParkedVehicle Model

---
+++ MVC_Garage 2.0/Models/ParkedVehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Garage_2._0.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Bus,
+        Boat,
+        Airplane,
+        Motorcycle
+    }
+
+    public enum Color
+    {
+        Black,
+        Blue,
+        Red,
+        White,
+        Greeen
+    }
+
+
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        public int RegistrationNumber { get; set; }
+        public int NumberOfWheels { get; set; }
+        public string VehicleBrand { get; set; }
+        public string VehicleModel { get; set; }
+        public DateTime InDate { get; set; }
+        public VehicleType VehicleTYpe { get; set; }
+        public Color Color { get; set; }
+    }
+}
