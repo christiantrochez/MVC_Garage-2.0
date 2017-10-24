af4580f38bd96fb25f7430645eae1dc5afebb99d
User <User@DESK-HM2GJ32>
2017-10-24 13:30:07 +0200

    ChangesFromDevToMaster/Afram

--- MVC_Garage 2.0/Controllers/ParkedVehiclesController.cs
+++ MVC_Garage 2.0/Controllers/ParkedVehiclesController.cs
@@ -15,15 +15,14 @@ namespace MVC_Garage_2._0.Controllers
     public class ParkedVehiclesController : Controller
     {
         private RegisterContext db = new RegisterContext();
-
+        private int MinuteCost = 10;
         // GET: ListAllVehicles
         public ActionResult ListAllVehicles(string sortOrder, string searchString, string currentFilter)
         {
             ViewBag.CurrentSort = sortOrder;
             ViewBag.RegSortParam = String.IsNullOrEmpty(sortOrder) ? "regNo_desc" : "";
             ViewBag.DateSortParam = sortOrder == "Date" ? "date_desc" : "Date";
-
-
+           
             var allParkedVehicles = db.ParkedVehicles;
 
             var VehicleItems = allParkedVehicles.Select(v => new VehicleListItem
@@ -45,6 +44,7 @@ namespace MVC_Garage_2._0.Controllers
             if (!String.IsNullOrEmpty(searchString))
             {
                 VehicleItems = VehicleItems.Where(v => v.RegistrationNumber.StartsWith(searchString));
+               
             }
 
             switch (sortOrder)
@@ -167,48 +167,57 @@ namespace MVC_Garage_2._0.Controllers
             return View(parkedVehicle);
         }
 
+             
         // POST: ParkedVehicles/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
-        //    db.ParkedVehicles.Remove(parkedVehicle);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-
-        //}
         public ActionResult DeleteConfirmed(int id)
         {
-            int price = 2;
-            int totalCost;
             ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
-            VehicleDeleteViewModel vehVM = new VehicleDeleteViewModel();
-            vehVM.Id = parkedVehicle.Id;
-            
-            //ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
-            //db.ParkedVehicles.Remove(parkedVehicle);
-            //db.SaveChanges();
-            //return RedirectToAction("Index");
-
-            var vehicle = new ParkedVehicle
-            {
-                Id = vehVM.Id,
-                InDate = vehVM.CheckInTime
-
-
-            };
-
-            var CheckOutDate = DateTime.Now;
-            TimeSpan totalTime = CheckOutDate.Subtract(vehicle.InDate);
-            totalCost = totalTime.Hours * price;
-            vehVM.kvittoString = $"Your Vehicle with Registration Number: {vehicle.RegistrationNumber} parked since {vehicle.InDate} until {CheckOutDate} will cost: {totalCost:C}";
-            //irmDelete.kvittoString
-            //return RedirectToAction("ConfirmDelete",kvittoString);
-            ViewBag.Text = vehVM.kvittoString;
+            db.ParkedVehicles.Remove(parkedVehicle);
+            db.SaveChanges();
             return RedirectToAction("Index");
-        }
 
+        }
+        //// GET: ParkedVehicles/Delete/5
+        public ActionResult CheckOut(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
+            if (parkedVehicle == null)
+            {
+                return HttpNotFound();
+            }
+            var allParkedVehicles = db.ParkedVehicles;
+
+            var RecieptVehicle = allParkedVehicles.Find(id);
+            Receipt recVehicle = new Receipt();
+            recVehicle.Id = RecieptVehicle.Id;
+            recVehicle.RegistrationNumber = RecieptVehicle.RegistrationNumber;
+            recVehicle.CheckInDate = RecieptVehicle.InDate;
+            recVehicle.CheckOutDate = DateTime.Now;
+            recVehicle.CostPerMinute = MinuteCost;
+            recVehicle.TotalParkedTime = (int)DateTime.Now.Subtract(RecieptVehicle.InDate).TotalMinutes;
+            recVehicle.TotalCost = recVehicle.TotalParkedTime * MinuteCost;
+
+            return View(recVehicle);
+          
+        }
+        // POST: ParkedVehicles/Delete/5
+        [HttpPost, ActionName("CheckOut")]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckOutConfirmed(int id)
+        {
+            ParkedVehicle parkedVehicle = db.ParkedVehicles.Find(id);
+            db.ParkedVehicles.Remove(parkedVehicle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+       
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -217,16 +226,6 @@ namespace MVC_Garage_2._0.Controllers
             }
             base.Dispose(disposing);
         }
-        public class VehicleDeleteViewModel
-        {
-            public int Id { get; set; }
-            public int Price { get; set; }
-            public DateTime CheckInTime { get; set; }
-            // public DateTime CheckOutTime { get; set; }
-            public string kvittoString;
-            // public int Count { get; set; }
-            //public string Description { get; set; }
-            //TimeSpan totalTime = DateTime.Now.Subtract(InDate);
-        }
+      
     }
 }
---
+++ MVC_Garage 2.0/Models/ViewModels/Receipt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Garage_2._0.Models.ViewModels
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Registration Number")]
+        public string RegistrationNumber { get; set; }
+
+        [Display(Name = "Check-Out Time")]
+        public DateTime CheckOutDate { get; set; }
+
+        [Display(Name = "Check-In Time")]
+        public DateTime CheckInDate { get; set; }
+
+        [Display(Name = "Parking Cost/Min")]
+        public int CostPerMinute { get; set; }
+
+        [Display(Name = "Total Parked Time")]
+        public int TotalParkedTime { get; set; }
+
+        [Display(Name = "Total Cost")]
+        public int TotalCost { get; set; }
+    }
+}
